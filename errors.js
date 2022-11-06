425abdd3d9f37f3c43bb3a1410cdaff541d19755
fede shierson <fedesh_98@live.com.ar>
2022-11-06 08:52:21 -0300

    avance}

--- errors.js
+++ errors.js
@@ -1,10 +1,158 @@
-// const fs = require('fs')
+const { write } = require('fs');
+const {readFile, writeFile, appendFile} = require('fs/promises');
 
-// try{
-//     fs.writeFileSync('./index.html',new Date().toLocaleString())
-// } catch (err) {
-//     throw new Error('Error en la escritura'+ err)
-// }
+class producto {
+    constructor(title, price, thumbnail){
+        this.title = title      
+        this.price = price 
+        this.thumbnail = thumbnail      
+       
+    }
+}
+class Contenedor{
+    
+
+    constructor(_file){
+        this.file = _file;
+    }
+
+
+    async getAll(){
+       
+        try{
+    
+            const result = await readFile(this.file, 'utf8');         
+            const object = JSON.parse(result);
+            return object;
+    
+        }catch(error){
+    
+        }
+           
+    }
+
+    async getById(_id){
+       
+        try{
+    
+            const result = await readFile(this.file, 'utf8');         
+            const object = JSON.parse(result);
+            let item = object.find(x => x.id == _id );
+            if(!item){
+                return null;
+            }
+            return item;
+    
+        }catch(error){
+    
+        }
+           
+    }
+
+    async Save(_producto){
+
+
+        try{
+    
+            const result = await readFile(this.file, 'utf8');         
+            const array = JSON.parse(result);
+            let lastItem = array[array.length - 1];
+            let id = 1;
+            if(lastItem){
+                                  
+                id = lastItem.id + 1; 
+                _producto.id = id;
+                array.push(_producto); 
+                await writeFile(this.file, JSON.stringify(array,null, 2));
+                  
+            }else{
+                _producto.id = id ;
+                let arr = [_producto];
+                let pJson = JSON.stringify(arr); 
+              
+                await writeFile(this.file, pJson);
+
+                
+            } 
+
+            return id;
+    
+            
+           
+    
+        }catch(error){
+      
+         
+            return 0;
+        }
+           
+    }
+
+
+}
+
+
+
+
+let contenedor = new Contenedor('productos.json');
+//contenedor.getAll().then(x => console.log(x));
+
+let producto1 = new producto("Soja",1450,"htttps");
+//producto1.id = 1
+
+//console.log(JSON.stringify(producto1));
+
+contenedor.Save(producto1).then(x => console.log(x));
+//contenedor.getById(2).then(x => console.log(x));
+/* const leerArchivo = async()=>{
+    
+    try{
+    
+        const result = await readFile('productos.txt', 'utf8')
+       console.log(result)
+
+    }catch(error){
+
+    }
+    
+}
+
+leerArchivo(); */
+
+
+
+
+
+
+/* 
+const fs = require('fs')
+
+
+try{
+ // Use fs.readFile() method to read the file
+fs.readFile('productos.txt', 'utf8', (err, data) => {
+      
+    // Display the file content
+    console.log(data);
+}); */
+
+   /*      if( data == 0 ){
+             let id = 1
+             fs.writeFile('./Productos.txt', JSON.stringify({ id, title, price }))
+             console.log("agregue un objeto nuevo porque no habia nada")
+            }else{
+                let data = JSON.parse( fs.readFile( './Productos.txt', 'utf-8' ) )
+                let id = data.id + 1
+                fs.appendFile('./Productos.txt', JSON.stringify({ id, title, price }))
+                console.log("Ya tenia algo, agregue un producto nuevo")
+            } */
+    /*     } catch(err){
+            throw new Error(`Error en la escritura del archivo: ` + err)
+        } */
+
+
+
+
 
 
                  // ejercicio de la clase 
@@ -73,7 +221,7 @@ fs.promises.readFile('./info.txt', 'utf-8')
 
 
 //Entrega
-
+/*
 const fs = require('fs')
 
 try{
@@ -108,7 +256,7 @@ try{
             }
     }
 }
-
+*/
 /*
 class productos {
     constructor(title, price, thumbnail, id){
